96777d37125bc5e4b6fcc7d69b3991023a705ee4
Vedantjn <vedant.jain.indore@gmail.com>
2021-06-01 20:11:49 +0530

    Nirmohi1

---
+++ t1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int r;
+    for (int r =1 ; r <= 5; r++)
+    {
+        cout<<"*";
+        cout<<"*";
+        cout<<"*";
+        cout<<"*";
+        cout<<endl;
+       
+    };
+
+    return 0;
+}
+// ==========================================================================================
+
+// or
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int r;
+//     for (int r =1 ; r <= 5; r++)
+//     {
+//         cout<<"****";
+//         cout<<endl; 
+//     };
+
+//     return 0;
+// }
---
+++ t24.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        int one = 1;
+        for (int j = 1; j <= i; j++)
+        {
+            cout << one << " ";
+            one = one * (i - j) / j;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
